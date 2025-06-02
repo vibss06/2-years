2d505a669651eae98da7b8d4cbfe00242636f5c3
vibss06 <vibhanshugajalkar006@gmail.com>
2025-06-02 21:39:23 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,27 @@
+function showLove() {
+  const response = document.getElementById("response");
+  response.innerText = "I love so much My LOVE 💖💫";
+}
+
+
+
+
+
+  const bgMusic = document.getElementById('bgMusic');
+  const musicControl = document.getElementById('musicControl');
+
+  let isPlaying = false;
+
+  musicControl.addEventListener('click', () => {
+    if (isPlaying) {
+      bgMusic.pause();
+      musicControl.textContent = '🎵';
+    } else {
+      bgMusic.play();
+      musicControl.textContent = '⏸️';
+    }
+    isPlaying = !isPlaying;
+  });
+
+
+
